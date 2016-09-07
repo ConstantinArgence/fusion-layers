b785a7662b439f4616427de650c71d47f277ef7c
ConstantinArgence <constantin.argence@gmail.com>
2016-09-07 09:15:21 +0200

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -132,6 +132,7 @@ var MapsLib = {
     max: 60,
     values: [0, 60],
     step: 5,
+    data: {fusionTableId}
     slide: function (event, ui) {
         $("#Km-selected-start").html(ui.values[0]);
         $("#Km-selected-end").html(ui.values[1]);
